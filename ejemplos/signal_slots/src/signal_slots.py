655f3bb732ddd4d47bf350bb999fa7892199f675
Lucas Kammann <lucaskammann@hotmail.com>
2019-11-08 01:53:48 -0300

    Agregando Slots, Signals y Properties a la guia

--- ejemplos/signal_slots/src/signal_slots.py
+++ ejemplos/signal_slots/src/signal_slots.py
@@ -10,11 +10,14 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
+		
         self.setWindowTitle("signal-slots 101")         # cambiamos el titulo de la ventana
+		
         self.press_button.setText("Don't press me")     # texto inicial del boton
+        self.pressed = False
+
         """ACA ESTA LA MAGIA: Asociamos el evento click del boton con el metodo button_pressed"""
         self.press_button.clicked.connect(self.button_pressed)
-        self.pressed = False
 
     def button_pressed(self):
         self.pressed = not self.pressed     # togglea el estado y muestra el texto correspondiente
