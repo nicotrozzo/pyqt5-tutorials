42cd2fb17f4f33a8036797c7eb43b1bafaf4dc96
nicotrozzo <rtrozzo@itba.edu.ar>
2020-08-15 17:34:25 -0300

    Fixed indentation problem on input_fetching

--- ejemplos/input_fetching/src/input_fetch.py
+++ ejemplos/input_fetching/src/input_fetch.py
@@ -10,7 +10,7 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)	# como siempre, inicializamos todo
-		
+
         self.setWindowTitle("InputFetching")	# titulo de la ventana
         self.input_line.textChanged.connect(self.update_bar)	# CADA VEZ que cambie el texto se llamara al callback
         """ El QIntValidator no deja que ingresen nada mas que numeros enteros.
@@ -24,12 +24,12 @@ class MyWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         text = self.input_line.text()	# levanto entrada que acaban de ingresar
         if text:	# si hay algo escrito
             """ Puedo castear directo a int porque el validator solo permite que ingresen numeros enteros """
-            input = int(self.input_line.text())	
+            input = int(self.input_line.text())
             if input:	# si el valor ingresado es 0 no hago nada
                 self.capacity_bar.setValue(1/input**2*100)	# cambia la barra de capacidad en funcion de la entrada
                 if self.capacity_bar.value() == 0:	# si llego a cero, muestro el mensaje oculto
                     self.msg_oculto.setText("Tenes la misma capacidad neuronal que yo cuando escribi esto :)")
-					self.msg_oculto.show()
+                    self.msg_oculto.show()
                 else:
                     self.msg_oculto.hide()
         else:	# si no ingresaron nada seteo 100%
