5c3a2a8d779ee2f9fe980435484ff219f7a712bb
Kammann123 <lucaskammann@hotmail.com>
2020-08-24 23:27:22 -0300

    Agregando soporte para promocion de widgets, warning y errores de compilacion.

--- tools/kt.py
+++ tools/kt.py
@@ -150,60 +150,111 @@ def compile(args):
     DESIGNER_COMPILED_PATH = os.path.join("src", "ui")
     RESOURCES_COMPILED_PATH = os.path.join("src", "resources")
 
-    # Verifico existencia del directorio designer
+    designer_qrc_files = []
+    designer_ui_files = []
+    custom_files = []
+    good = False
+
+    # [1] Proceso de verificacion de las carpetas correspondientes a la estructura de proyecto asumida
     if os.path.exists(os.path.join(CURRENT_PATH, DESIGNER_PATH)):
-        # Compilando cada archivo .ui
-        designer_files = os.listdir(os.path.join(CURRENT_PATH, DESIGNER_PATH))
-        log(f'Se encontraron {len(designer_files)} archivos .ui en /designer')
-        for designer_file in designer_files:
-            log(f'Compilando "{designer_file}"...')
-            os.system(f'pyuic5 -x {os.path.join(DESIGNER_PATH, designer_file)} -o {os.path.join(DESIGNER_COMPILED_PATH, f"{designer_file[:-3]}.py")}')
-    else:
-        log('No se pudo encontrar la carpeta /designer')
-    
-    # Verifico existencia del directorio resources
-    if os.path.exists(os.path.join(CURRENT_PATH, RESOURCES_PATH)):
-        # Compiling the .qrc files, getting their names and running the command ony by one
-        resources_files = os.listdir(os.path.join(CURRENT_PATH, RESOURCES_PATH))
-        compiled_resources = []
-        log(f'Se encontraron {len(resources_files)} archivos .qrc en /resources')
-        for resource_file in resources_files:
-            log(f'Compilando "{resource_file}"...')
-            compiled_resource = os.path.join(RESOURCES_COMPILED_PATH, f"{resource_file[:-4]}_rc.py")
-            compiled_resources.append(f"{resource_file[:-4]}_rc.py")
-            os.system(f'pyrcc5 {os.path.join(RESOURCES_PATH, resource_file)} -o {compiled_resource}')
-    else:
-        log('No se pudo encontrar la carpeta /resources')
-    
-    # Corrigiendo el problema del import del modulo de recursos desde un archivo designer
-    # con un path incorrecto, generado automaticamente por QtDesigner
-    log('Corrigiendo import de recursos en archivos compilados...')
-    for designer_file in designer_files:
-        filename = os.path.join(CURRENT_PATH, DESIGNER_COMPILED_PATH, f"{designer_file[:-3]}.py")
-        content = ""
-        rewrite = False
-
-        file = open(filename, 'r')
-        for line in file:
-            if '\n' in line:
-                line = line[:-1]
-
-            for resource in compiled_resources:
-                if line == f'import {resource[:-3]}':
-                    content += f'from src.resources import {resource[:-3]}\n'
-                    rewrite = True
-                    break
+        if os.path.exists(os.path.join(CURRENT_PATH, RESOURCES_PATH)):
+            if os.path.exists(os.path.join(CURRENT_PATH, DESIGNER_COMPILED_PATH)):
+                if os.path.exists(os.path.join(CURRENT_PATH, RESOURCES_COMPILED_PATH)):
+                    log('Estructura de proyecto reconocida correctamente.')
+                    good = True
+                else:
+                    log('¡Error! Estructura de proyecto - No se encontro la carpeta /src/resources')
             else:
-                content += f'{line}\n'
-        file.close()
+                log('¡Error! Estructura de proyecto - No se encontro la carpeta /src/ui')
+        else:
+            log('¡Error! Estructura de proyecto - No se encontro la carpeta /resources')
+    else:
+        log('¡Error! Estructura de proyecto - No se encontro la carpeta /designer')
 
-        if rewrite:
-            file = open(filename, 'w')
-            file.write(content)
+    if good:
+
+        # [2] Compilando cada archivo *.ui detectado en la carpeta /designer y registrandolo
+        _files = os.listdir(os.path.join(CURRENT_PATH, DESIGNER_PATH))
+        for _file in _files:
+            _filename, _ext = os.path.splitext(_file)
+            if _ext == '.ui':
+                designer_ui_files.append(_filename)
+                log(f'Compilando "{_filename}.ui"')
+                os.system(f'pyuic5 -x {os.path.join(DESIGNER_PATH, f"{_filename}.ui")} -o {os.path.join(DESIGNER_COMPILED_PATH, f"{_filename}.py")}')
+        log(f'Se compilaron {len(designer_ui_files)} archivos *.ui')
+
+        # [3] Compilando cada archivo *.qrc detectado en la carpeta /resources y registrandolo
+        _files = os.listdir(os.path.join(CURRENT_PATH, RESOURCES_PATH))
+        for _file in _files:
+            _filename, _ext = os.path.splitext(_file)
+            if _ext == '.qrc':
+                designer_qrc_files.append(_filename)
+                log(f'Compilando "{resource_file}.qrc"')
+                os.system(f'pyrcc5 {os.path.join(RESOURCES_PATH, f"{_filename}.qrc")} -o {os.path.join(RESOURCES_COMPILED_PATH, f"{_filename}_rc.py")}')
+        log(f'Se compilaron {len(designer_qrc_files)} archivos *.qrc')
+
+        # [4] Busco archivos Widget personalizados en /src
+        _files = os.listdir(os.path.join(CURRENT_PATH, 'src'))
+        for _file in _files:
+            _filename, _ext = os.path.splitext(_file)
+            if _ext == '.py':
+                if _filename not in designer_ui_files:
+                    if _filename not in ['app', 'mainwindow', '__init__']:
+                        custom_files.append(_filename)
+
+        # [5] Corrección de direcciones relativas para importar módulos desde QtDesigner
+        for designer_ui_file in designer_ui_files:
+            filename = os.path.join(CURRENT_PATH, DESIGNER_COMPILED_PATH, f"{designer_ui_file}.py")
+            content = ""
+            rewrite = False
+
+            file = open(filename, 'r')
+            for line in file:
+                replaced = False
+
+                if '\n' in line:
+                    line = line[:-1]
+
+                if 'import' in line and 'PyQt5' not in line:
+                    # [5.a] Posible import de un recurso
+                    for designer_qrc_file in designer_qrc_files:
+                        target = f'import {designer_qrc_file}_rc'
+                        if line == target:
+                            content += f'from src.resources import {designer_qrc_file}_rc\n'
+                            if not rewrite:
+                                rewrite = True
+                            replaced = True
+                            break
+
+                    # [5.b] Posible import de un widget promovido
+                    for ui_file in designer_ui_files + custom_files:
+                        if ui_file != designer_ui_file:
+                            target = f'from {ui_file} import {ui_file}'
+                            if line == target:
+                                content += f'from src.{ui_file} import {ui_file}\n'
+                                if not rewrite:
+                                    rewrite = True
+                                replaced = True
+                                break
+                    
+                    # [5.c] Posible import no corregido
+                    if not replaced:
+                        if '_rc' in line:
+                            log(f'\t¡Advertencia! Módulo "{line}" - Posible .qrc faltante o no detectado, pero usado en QtDesigner, y no se pudo corregir el import.')
+                        else:
+                            log(f'\t¡Advertencia! Módulo "{line}" - Posible archivo widget faltante en src/ui usado en QtDesigner como Promovido, no se pudo corregir el import.\n \t\tEn caso de ser módulo nativo de Python, ignorar, no soy muy inteligente aún.')
+                
+                if not replaced:
+                    content += f'{line}\n'
             file.close()
-    
-    # Termine!
-    log('Compilación finalizada con éxito!')
+
+            if rewrite:
+                file = open(filename, 'w')
+                file.write(content)
+                file.close()
+        
+        # Termine!
+        log('Compilación finalizada con éxito!')
 
 def help(args):
     """ Muestra en consola un texto de ayuda.
